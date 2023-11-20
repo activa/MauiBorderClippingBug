93d2bd0709787580fa5d2f32c41746ddf27f79c7
Philippe Leybaert <pleybaert@agileassets.com>
2023-11-19 17:14:38 -0700

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BorderClippingBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace BorderClippingBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindableLayout.SetItemsSource(layout, new[] { 0, 1, 2, 4, 8, 16, 32 });
+        }
+
+    }
+}
